cb45ab3d7430a16092fcd1116da6b991b49d784d
Reina <two.oao@gmail.com>
2025-09-26 17:54:52 +0800

    update

--- js/swiper.js
+++ js/swiper.js
@@ -1,18 +1,16 @@
-// core version + navigation, pagination modules:
 import Swiper from "swiper";
-import { Navigation, Pagination } from "swiper/modules";
-// import Swiper and modules styles
+import { Navigation, Pagination, Grid } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
+import "swiper/css/grid";
 
-// init Swiper:
-const swiper = new Swiper(".swiper", {
+// swiper1
+const swiper1 = new Swiper(".swiper1", {
   // configure Swiper to use modules
   modules: [Navigation, Pagination],
   // centeredSlides: false,
   // loop: true,
-
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
@@ -37,6 +35,27 @@ const swiper = new Swiper(".swiper", {
       centeredSlides: true,
       loop: false,
       slidesPerView: "1.2",
+    },
+  },
+});
+
+// swiper2
+const swiper2 = new Swiper(".swiper2", {
+  modules: [Pagination, Grid],
+  pagination: {
+    el: ".swiper2-pagination",
+  },
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      spaceBetween: 24,
+      grid: {
+        rows: 2,
+      },
+    },
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 24,
     },
   },
 });
