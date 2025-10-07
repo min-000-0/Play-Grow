85de67b3123504dc2c77a7f67ddcdd261191b6ba
Reina <two.oao@gmail.com>
2025-10-07 17:47:29 +0800

    update

--- js/favBtn.js
+++ js/favBtn.js
@@ -1,7 +1,10 @@
 const favBtns = document.querySelectorAll(".favBtn");
 
 favBtns.forEach((favBtn) => {
-  favBtn.addEventListener("click", () => {
+  favBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+
     const icon = favBtn.querySelector("i");
     icon.classList.toggle("fi-rr-heart");
     icon.classList.toggle("fi-sr-heart");
